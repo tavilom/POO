60526d7239da070c20e1819fcddaadd4cce5e2e1
Otavio Mastrantonio <111095614+tavilom@users.noreply.github.com>
2022-11-18 12:08:30 -0300

    Update aluno.ts
    Adicionado perna e peito

--- src/aluno.ts
+++ src/aluno.ts
@@ -3,8 +3,8 @@ import { Util } from './util';
 
 export abstract class Aluno extends Pessoa {
   constructor(nome: string, cpf: string) {
-    super(nome + "aluno", cpf + "123.456.789-12", 100, 0)
-    this._vigor = this._cancaco
+    super(nome + "aluno", cpf + "123.456.789-12", 100, 0, 100)
+    this._vigor = this._cansaco
   }
   protected _turma!: String;
 
@@ -13,20 +13,42 @@ export abstract class Aluno extends Pessoa {
   }
 
   public correr(aluno: Pessoa): void{
-    this._vigor -= Math.random();
-    this._cancaco += Math.random();
-     if(this._cancaco > this._vigor) {
-      console.log("Você passou mal");
+    this._vigor -= Math.random() / 100;
+    this._cansaco += Math.random() * 10;
+     if(this._cansaco >= this._vigor) {
+      console.error("Você passou mal!");
+      ;
      } else {
-      if (this._vigor > this._cancaco) {
+      if (this._vigor > this._cansaco) {
         console.log("Cardio completo!");
       }
      }
   }
 
   public perna(aluno: Pessoa): void {
+    this._vigor -= Math.random() / 100;
+    this._cansaco += Math.random() * 10;
+    this._musculo -= Math.random() * 10;
+    if(this._cansaco >= this._vigor) {
+      console.error("Você passou mal!");
+    } else if (this._musculo <= 10) {
+      console.error("Você se machucou");
+    } else {
+      console.log("Treino de perna completo!");
+    }  
+  }
 
-      
+  public peito(aluno: Pessoa): void {
+    this._vigor -= Math.random() / 100;
+    this._cansaco += Math.random() * 10;
+    this._musculo -= Math.random() * 10;
+    if(this._cansaco >= this._vigor) {
+      console.error("Você passou mal!");
+    } else if (this._musculo <= 10) {
+      console.error("Você se machucou");
+    } else {
+      console.log("Treino de peito completo!");
+    }  
   }
 
 }
