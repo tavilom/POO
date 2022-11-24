5bd8d7772b32b266ae4d86f0f58305df7e6d4e2a
Otavio Mastrantonio <111095614+tavilom@users.noreply.github.com>
2022-11-24 10:34:39 -0300

    Update aluno

--- src/aluno.ts
+++ src/aluno.ts
@@ -1,7 +1,6 @@
 import {Pessoa} from './pessoa'
 import { Util } from './util';
 
-import { Pessoa } from './pessoa'
 
 export class Aluno extends Pessoa {
   constructor(
@@ -9,40 +8,68 @@ export class Aluno extends Pessoa {
     idade: number,
     cpf: string,
     genero: string,
-    protected _forca: number,
-    protected _cansaco: number,
-    protected _peso: number,
-    protected _intervalo: number
+    
 
   ) {
-  super(nome, idade, cpf, genero)
+  super(nome, idade, cpf, genero, 100, 100, 0, 100, 0)
   }
-}
+
 
   public falar(): void {
     console.log(`${this._nome} Bom dia! Estou pronto para o treinamento de hoje.`);
   }
 
   public correr(aluno: Pessoa): void{
-        this._forca -= Math.random() - 100;
-        this._cansaco += Math.random() * 10;
-         if(this._cansaco >= this._forca) {
+        this._forcaAtual -= Math.random() / 0.05;
+        this._cansaco += Math.random() * 0.05;
+         if(this._cansaco >= this._forcaAtual) {
           console.error("Você passou mal!");
           ;
          } else {
-          if (this._forca > this._cansaco) {
+          if (this._forcaAtual > this._cansaco) {
             console.log("Cardio completo!");
           } 
       } 
 }
 
-public malhar(): void{}
+public malharPerna(): void{
+  const certo = this._forcaAtual > this._peso;
+  if(certo) {
+    const perdaforca: number =  (this._forcaAtual / 0.05 && this._cansaco / 0.02)
+    console.log(`Status atula do aluno: ${perdaforca}`);
+  } else {
+    throw new Error(`${this._nome} se machucou e teve que ir para casa`);
+  }
+}
+
+public malharPeito(): void{
+  const certo = this._forcaAtual > this._peso;
+  if(certo) {
+    const perdaforca: number =  (this._forcaAtual / 0.05 && this._cansaco / 0.02)
+    console.log(`Status atula do aluno: ${perdaforca}`);
+  } else {
+    throw new Error(`${this._nome} se machucou e teve que ir para casa`);
+  }
+}
+public malharOmbro(): void{
+  const certo = this._forcaAtual > this._peso;
+  if(certo) {
+    const perdaforca: number =  (this._forcaAtual / 0.05 && this._cansaco / 0.02)
+    console.log(`Status atula do aluno: ${perdaforca}`);
+  } else {
+    throw new Error(`${this._nome} se machucou e teve que ir para casa`);
+  }
+}
 
 public intervalo(): void{
+  this._forcaAtual += this._forcaAtual * 1.05;
+  if (this._forcaAtual > this._forca) {
+    this._forcaAtual = this._forca
+  }
 }
 
-}
 
+}
 
 // //aqui pra baixo é tudo comentario
 // export abstract class Aluno extends Pessoa {
@@ -93,3 +120,4 @@ public intervalo(): void{
 //   }
 
 // }
+
